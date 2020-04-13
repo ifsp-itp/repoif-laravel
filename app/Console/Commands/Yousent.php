790cc52b14fd26358d9a8b9d4a4e43e78f23a94a
Pedro-ls <pedrohenriquedasilva204@gmail.com>
2020-04-13 16:26:16 -0300

    arquivo modificado para enviar para o youtube colocado variaveis foo bar baz

--- app/Console/Commands/Yousent.php
+++ app/Console/Commands/Yousent.php
@@ -2,10 +2,27 @@
 
 namespace App\Console\Commands;
 
+use Exception;
+use App\Project;
+use Illuminate\Support\Facades\File;
+
 use Illuminate\Console\Command;
+use Dawson\Youtube\Facades\Youtube;
+use Illuminate\Support\Facades\Storage;
 
 class Yousent extends Command
 {
+
+    /*
+    *criar variavel CRON_YOUTUBE_PATH 
+    *no arquivo .env e colocar o path do caminho 
+    *até seus arquivos
+    *ex: meus arquivos estão storage/files ou storage/app/public/files
+    *ou caso seu arquivo esteja executando em um servidor [caminho absoluto] caminho completo
+    *até a pasta de seus arquivos
+    */
+
+
     /**
      * The name and signature of the console command.
      *
@@ -38,41 +55,68 @@ class Yousent extends Command
     public function handle()
     {
 
+        //acha videos não enviados
         $naoEnviados = Project::where(
         'sent', 0)->get();
-
         $contador = $naoEnviados->count();
 
+        //looping que envia videos
         while ($contador > 0) {
 
             $upload = $naoEnviados->where('sent', '0')->first->id;
+         
+            //verifica se éum video [type igual a 2]
+            if($upload->type == 2){
+                //caminho do arquivo presente dentro de storage/files
+                foreach (File::files(env('CRON_YOUTUBE_PATH')) as $value) {
+                    //acha video na pasta
+                    if($value->getFilename() == $upload->project){
+                        $file = $value;
+                    }
+                }
+             try {
+                    //faz upload
+              
+                    $video = Youtube::upload($file, [
+                        'title'       => $upload->title,
+                        'description' => $upload->description,
+                        'tags'        => ['foo', 'bar', 'baz'],
+                        'category_id' => $upload->type
+                    ]);
+                    //busca dados do youtube
+                    $snippet = $video->getSnippet();
+                    $thumbnailURL = $snippet->thumbnails->high->url;
+                    $nameFile = $video->getVideoId();
+                    
+                    //update dos dados do youtube
+                    $upload->sent = '1';
+                    $upload->thumbnailURL = $thumbnailURL;
+                    $upload->project = $nameFile;
+                    $upload->save();
 
-            $arquivo = "storage/files/$upload->project";
-
-            try {
-
-                $video = Youtube::upload($arquivo, [
-                'title'       => $upload->title,
-                'description' => $upload->description,
-                //'tags'        => $upload->title,
-                'category_id' => $upload->type
-                ]);
-                $snippet = $video->getSnippet();
-                $thumbnailURL = $snippet->thumbnails->high->url;
-                $nameFile = $video->getVideoId();
-
-                $upload->sent = '1';
-                $upload->thumbnailURL = $thumbnailURL;
-                $upload->project = $nameFile;
-                $upload->save();
-
-                unlink($arquivo);  
-
-            } 
-            catch(Exception $e)
-            {
-                echo "A Fila não pode detectar video disponivel";
+                    echo "video ( ".$nameFile." ) enviado sucesso!!!\n";
+                    
+                    //update video
+                    $naoEnviados = Project::where(
+                        'sent', 0)->get();
+                    $contador = $naoEnviados->count();
+                } 
+                catch(Exception $e)
+                {
+                    dd($e);
+                    
+                    //quando não for mais possivel enviar videos
+                    //mostra mensagem
+                    echo "A Fila não pode continuar uploads \n";
+                    break;
+                    //sai do looping
+                    
+                }
             }
+        
+          
+
+            
         }
     }
 }
