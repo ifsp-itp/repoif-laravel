513dc140b8e6ccf8ca41cba1aaf44cad3f879d43
Pedro-ls <pedrohenriquedasilva204@gmail.com>
2020-08-29 18:41:52 -0300

    tirando bugs do upload

--- resources/views/project/create.blade.php
+++ resources/views/project/create.blade.php
@@ -13,7 +13,7 @@
     <title>REPOIF</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ asset('js/app.js') }}" defer></script> -->
     
     <!-- Progress Bar -->
    
